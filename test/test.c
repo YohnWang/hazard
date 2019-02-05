890e9b7fda109c36ecd778999954947d6d1b0511
YohnWang <347220312@qq.com>
2019-02-05 22:03:01 +0800

    more tests

--- test/test.c
+++ test/test.c
@@ -4,11 +4,17 @@
 
 static void test_mem()
 {
+    int x,y,z;
     test_mem_init(0x10000);
-    mem_sw(77777,100);
-    int x;
-    mem_lw(&x,100);
-    fprintf(stderr,"%d\n",x);
+    mem_sb(2,32);
+    mem_sh(5,64);
+    mem_sw(2019,100);
+    mem_lb(&x,32);
+    mem_lh(&y,64);
+    mem_lw(&z,100);
+    fprintf(stderr,"%d ",x);
+    fprintf(stderr,"%d ",y);
+    fprintf(stderr,"%d\n",z);
     test_mem_del();
 }
 
